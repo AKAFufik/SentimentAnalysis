899c28179d962ec65573c1e31c247c3d2fb7b22e
Artem Fufurin <artem.fufurin@hotmail.com>
2023-04-15 15:36:45 +0300

    Добавлен словарь и реализован метод для определения тональности текста

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace SentimentAnalysis
 {
@@ -10,6 +9,32 @@ namespace SentimentAnalysis
     {
         static void Main(string[] args)
         {
+            // Загрузка файла словаря AFINN в формате json
+            string afinnFilePath = "D:\\Документы\\Program\\SentimentAnalysis\\AFINN-ru.json";
+            JObject afinn = JObject.Parse(File.ReadAllText(afinnFilePath));
+            // Тестирование метода для определения тональности текста на основе словаря AFINN
+            string text1 = "Да .Этот фильм был просто ужасный и жалобный";
+            string text2 = "Я очень люблю этот ресторан";
+            int score1 = GetSentimentScore(text1, afinn); // score1 = -3
+            int score2 = GetSentimentScore(text2, afinn); // score2 = 3
+            Console.WriteLine(score1);
+            Console.WriteLine(score2);
+        }
+
+        public static int GetSentimentScore(string text, JObject afinn)
+        {
+            int score = 0;
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (afinn.TryGetValue(word, out JToken value))
+                {
+                    score += value.Value<int>();
+                }
+            }
+
+            return score;
         }
     }
 }
